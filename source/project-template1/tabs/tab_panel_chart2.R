3cafefe422bb524569fca7d71896b1c92e1516d1
juseylee <seylee@uw.edu>
2022-12-05 20:15:37 -0800

    chart 2

--- source/project-template1/tabs/tab_panel_chart2.R
+++ source/project-template1/tabs/tab_panel_chart2.R
@@ -3,6 +3,36 @@
 library(shiny)
 
 tab_panel_chart2 <-tabPanel(
-    "Chart 2",
-    p("This is chart 2.")
+  "Chart 2",
+  hr(),
+  fluidPage(
+    h2("Percentage of Male voted vs Percentage of Female voted"),
+    hr()
+  ),
+  fluidRow(
+    column(12,
+           
+           #Create a select box
+           selectInput(
+             "Gender", "Choose a gender", Gender
+           )),
+    hr(),
+  ),
+  fluidPage(
+    plotlyOutput("bar_chart"),
+    hr(),
+  ),
+  fluidPage(
+    h3("Purpose"),
+    p("The purpose of this chart is to compare the percentage of votes between gender in the United States in November 2020. 
+      The distribution of this data set is into many different categories, such as gender, race, Hispanic origin, total percentage of registration, 
+      total percentage of votes, and more. For that reason, we thought it would be interesting to make a distribution in a bar chart that shows 
+      the comparison of votes in gender by the state in the United States."),
+    h3("Observations"),
+    p("Through this chart, we can determine the gender participation in voting by states in the United States. 
+    For example, we can notice that the District of Columbia had the highest voting participation, and the females voted more, 
+    with 79.5%, while 75.5% of males voted. Also, we can observe that Arkansas had the lowest participation, and the females voted more, 
+    with 54.1% and 54.1% of males voting. Overall, females have participated more than males in voting. ")
+  )
 )
+
