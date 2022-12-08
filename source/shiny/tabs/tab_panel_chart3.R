8efdaa490a2bbd62328ad05027fd42aa097e9ee4
sukhd12 <skd26@uw.edu>
2022-12-07 18:42:28 -0800

    chart 3

--- source/shiny/tabs/tab_panel_chart3.R
+++ source/shiny/tabs/tab_panel_chart3.R
@@ -35,9 +35,12 @@ tab_panel_chart3 <-tabPanel(
     correlates with the type of candidate they vote for. We want to observe if there are, or aren't,
     any trends along all the counties in America. With this chart, we can choose a state and 
     clearly observe all the counties in that state and their voting patterns based on their 
-    median household income"),
+    median household income. We decided for our graph to focus on locations from different regions
+      of the United States to get a more clear visualization and see how they differ so
+      we only selected a few states from different regions to look at separately."),
     h3("Observations"),
-    p("After creating this map, something that can be seen quickly is the high number of 
-    voters at a median household income of between 40,000 - 50,000 dollars a year. This pattern
-    occurred in almost all states."))
+    p("After creating this chart, something that can be seen quickly is the high number of 
+    voters at a median household income of between $40,000 - $50,000 a year. Most of these populations had a higher percentage of votes for Trump.
+    Further, households with higher incomes tended to have a smaller percentage of votes go to Trump. This pattern
+    occurred in almost all states. "))
 )
