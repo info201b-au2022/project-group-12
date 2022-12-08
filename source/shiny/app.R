8c4dbbce315ae00dc39be3728c2de1c29957669a
elideng <elideng@uw.edu>
2022-12-07 19:57:17 -0800

    summary done

--- source/shiny/app.R
+++ source/shiny/app.R
@@ -6,7 +6,8 @@
 #
 #    http://shiny.rstudio.com/
 #
-
+# install.packages('rsconnect')
+library("rsconnect")
 library(shiny)
 source("app_ui.R")
 source("app_server.R")
--- source/shiny/app_server.R
+++ source/shiny/app_server.R
@@ -376,10 +376,64 @@ ken_graph <- ggplot(data = ken) +
   scale_color_manual(values = c("#00BA38")) +
   scale_x_continuous(labels = scales::comma)
 
+
+#------------------Summary Table---------------------------
+library("tidyverse")
+library("RcppRoll")
+library("knitr")
+
+data_orig <- read.csv('https://raw.githubusercontent.com/MEDSL/2018-elections-unoffical/master/election-context-2018.csv')
+
+View(data_orig)
+
+
+data <- select(data_orig, -c(10:21)) %>%
+  select(-c(27))
+
+data <- na.omit(data)
+
+total <- data$trump16 + data$clinton16 + data$otherpres16
+
+pct_trump <- round((data$trump16 / total) * 100, 2)
+
+pct_clinton <- round((data$clinton16 / total) * 100, 2)
+
+pct_other <- round((data$otherpres16 / total) * 100, 2)
+
+pres_16 <- transmute(data, State = data$state,
+                     County = data$county, Trump = pct_trump,
+                     Clinton = pct_clinton, Other = pct_other,
+                     White = data$white_pct,
+                     Non_White = data$nonwhite_pct,
+                     Foreign_born = data$foreignborn_pct,
+                     Female = data$female_pct,
+                     Under_29 = data$age29andunder_pct,
+                     Over_65 = data$age65andolder_pct,
+                     Median_Income = data$median_hh_inc,
+                     Rural = data$rural_pct) 
+
+summary_table <- pres_16 %>%
+  group_by(State) %>%
+  summarize(
+    Trump = round(mean(Trump), 1),
+    Clinton = round(mean(Clinton), 1),
+    White = round(mean(White), 1),
+    NonWhite = round(mean(Non_White), 1),
+    Foreign_Born = round(mean(Foreign_born), 1),
+    Female = round(mean(Female), 1),
+    Under_29 = round(mean(Under_29), 1),
+    Median_Income = round(mean(Median_Income), 1),
+    Rural = round(mean(Rural), 1)
+  )
+
+View(summary_table)
+
 #------------------- Server -----------------------------
 
 
 server <- function(input, output) {
+  
+  output$summary <- renderTable(summary_table)
   
   output$img1 <- renderImage({
     file <- normalizePath(file.path("./www/vote1.jpg"))
@@ -434,6 +488,8 @@ server <- function(input, output) {
   })
   
 }
+
+
 
 
 
